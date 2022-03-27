7f53f9bd990a472364c953af78cf926b97fee22d
quiquee <enrique.melero@gmail.com>
2022-03-27 14:52:52 +0200

    Looks for QR based on discordname#discriminator and discord id

--- qrDiscordBot.py
+++ qrDiscordBot.py
@@ -17,29 +17,46 @@ async def on_message(message):
     # If the author is the robot itself, then do nothing!
     if message.author == client.user:
         return
+    discordname = message.author.name + "#" + message.author.discriminator
+    discordid=str(message.author.id)
     # If the user writes $qr
     if message.content == "$qr":
         current_time = now.strftime("%H:%M:%S")
-        print('\n')
-        qrCodePath="QRCode_" + str(message.author.id) + ".png"
-        # This for loop check for all the user's DiscordID in the Database
-        if exists(qrCodePath) :
-            print("This user received his QR Code : " + message.author.name)
-            print("Discord ID : " + str(message.author.id))
-            print("Current time : ", current_time)
-            
-            # Send the QrCode the the user who asked for
-            await message.author.send(
-                "------------------------------------------------\n\n\nHello " + message.author.name + "\nHere is your new QR Code: ")
-            await message.author.send(file=discord.File(qrCodePath))
-            
-            return
+        print('QR Requested')
+        qrCodePath="NONE"
+        qrCodePath_name="qrcodes/QRCode_" + discordname + ".png"
+        qrCodePath_id="qrcodes/QRCode_" + discordid + ".png"
+        # This for loop check for all the user's DiscordID in the Database        
+        if exists(qrCodePath_id) :
+            print('Found QR by discord ID')
+            qrCodePath = qrCodePath_id
+        elif exists(qrCodePath_name) :
+            print('Found QR by discord name#discr')
+            qrCodePath = qrCodePath_name
         else:
+            print(current_time, "A user didn't receive a non existing QR Code : " + qrCodePath_name + " or " + qrCodePath_id)               
+            print("Discord ID : " + discordid)
+            print("Discord Name # Discr : " + discordname )
+            print("Current time : ", current_time)     
             await message.author.send(
                 "------------------------------------------------\n\n\nHello " + message.author.name + "\nSorry, there is no QR Code for you \n" +
-                "If you want to become a Plei Scholar, please register at https://plei.games/contacto/")
-            print(current_time, "A user didn't receive a non existing QR Code : " + qrCodePath)            
+                "If you want to become a Plei Scholar, please register on this website: https://plei.games/contacto/")                
             return
+
+        print(current_time, "This user received his QR Code : " + qrCodePath)
+        print("Discord ID : " + discordid)
+        print("Discord Name#Discriminator : " + discordname )
+        # Send the QrCode the the user who asked for
+        await message.author.send(
+            "------------------------------------------------\n\n\nHello " + message.author.name + "\nHere is your new QR Code: ")
+        await message.author.send(file=discord.File(qrCodePath))
+        
+        return
+                        
+            
+        
+        
+            
     
 
 #Run the client (This runs first)
